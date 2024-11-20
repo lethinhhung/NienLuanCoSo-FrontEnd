edc4e91569e8d5871eed5acb33b7734a5c945b7e
Kong Trua <thung260803@gmail.com>
2024-11-20 20:52:28 +0700

    #150: Major update

--- config-overrides.js
+++ config-overrides.js
@@ -1,28 +1,13 @@
-const { override, useBabelRc, addWebpackAlias } = require('customize-cra');
-const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
-const NodeSchemePlugin = require('./NodeSchemePlugin');
+const { override, addBabelPlugins } = require('customize-cra');
 
 module.exports = override(
-    useBabelRc(),
-    addWebpackAlias({
-        path: require.resolve('path-browserify'),
-    }),
-    (config) => {
-        config.resolve.fallback = {
-            ...config.resolve.fallback,
-            fs: false,
-            assert: require.resolve('assert/'),
-            os: require.resolve('os-browserify/browser'),
-            url: require.resolve('url/'),
-            buffer: require.resolve('buffer/'),
-            stream: require.resolve('stream-browserify'),
-            constants: require.resolve('constants-browserify'),
-            zlib: require.resolve('browserify-zlib'),
-            child_process: false,
-            http: require.resolve('stream-http'),
-            querystring: require.resolve('querystring-es3'),
-        };
-        config.plugins = (config.plugins || []).concat([new NodePolyfillPlugin(), new NodeSchemePlugin()]);
-        return config;
-    },
+    ...addBabelPlugins([
+        'module-resolver',
+        {
+            root: ['./src'],
+            alias: {
+                '~': './src',
+            },
+        },
+    ]),
 );
